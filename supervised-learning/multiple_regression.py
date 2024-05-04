b4f0ec115ee370d1dbe6c701d1d760401c871a83
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-05-04 15:08:17 -0400

    fix mistakes in multiple regression

--- supervised-learning/multiple_regression.py
+++ supervised-learning/multiple_regression.py
@@ -1,16 +1,12 @@
 import copy
 import numpy as np
 
-np.set_printoptions(precision=2)
-
 X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
 y_train = np.array([460, 232, 178])
 
 X_train = np.array([np.append(feature, 1) for feature in X_train])
 
-w_init = np.array(
-    [0.39133535, 18.75376741, -53.36032453, -26.42131618, 785.1811367994083]
-)
+w_init = np.array([1.0, 1.0, 1.0, 1.0, 1.0])
 
 
 def y_hat(x: np.ndarray, w: np.ndarray) -> float:
@@ -40,30 +36,26 @@ def gradient_descent(
     x: np.ndarray,
     y: np.ndarray,
     w_start: np.ndarray,
-    cost_function: callable,
-    gradient_function: callable,
     alpha: float,
     num_iters: int,
 ):
     cur_w = copy.deepcopy(w_start)
     cost_history = []
     for i in range(num_iters):
-        gradient_vector = gradient_function(x, y, w_start)
+        gradient_vector = compute_gradient(x, y, cur_w)
         cur_w -= alpha * gradient_vector
         if i < 100000:
-            cost_history.append(cost_function(x, y, cur_w))
+            cost_history.append(compute_cost(x, y, cur_w))
     return cur_w, cost_history
 
 
-w, cost_history = gradient_descent(
-    X_train, y_train, w_init, compute_cost, compute_gradient, 5.0e-7, 1000
-)
+w, cost_history = gradient_descent(X_train, y_train, w_init, 5.0e-7, 1_000_000)
 print(f"w vector found: {w}")
 
 print()
 
 for i in range(len(X_train)):
     print("actual")
-    print(y_hat(X_train[i], w))
-    print("predicted")
     print(y_train[i])
+    print("predicted")
+    print(y_hat(X_train[i], w))
