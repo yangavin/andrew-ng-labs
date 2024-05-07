79b88b59c58f0f847121e26a338f77ed7724d475
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-05-07 18:05:53 -0400

    apply regularized logistic regression on dataset2

--- supervised-learning/logistic_regression.py
+++ supervised-learning/logistic_regression.py
@@ -58,7 +58,7 @@ def gradient_descent(
     alpha: float,
     iters: int,
     lambda_: float = 0,
-) -> tuple[np.ndarray, list]:
+) -> np.ndarray:
     """
     Performs batch gradient descent to learn w. Updates w by taking
     iters gradient steps with learning rate alpha
@@ -77,25 +77,55 @@ def gradient_descent(
         cost_history: (list[float]) History of cost during descent
     """
     cur_w = copy.deepcopy(w_init)
-    cost_history = []
     for _ in range(iters):
         cur_w -= alpha * compute_gradient(x, y, cur_w, lambda_)
-        cost_history.append(compute_cost(x, y, cur_w, lambda_))
-    return cur_w, cost_history
+    return cur_w
 
 
+print("Performing unregularized logistic regression on ex2data1.txt")
 dataset = np.genfromtxt("data/ex2data1.txt", delimiter=",")
 x_train = dataset[:, :-1]
 x_train = np.array([np.append(example, 1) for example in x_train])
 y_train = dataset[:, -1]
-
 np.random.seed(1)
 initial_w = 0.01 * (np.random.rand(2).reshape(-1, 1) - 0.5)
 initial_w = np.append(initial_w, -8)
 iterations = 10000
 alpha = 0.001
-w, cost_history = gradient_descent(x_train, y_train, initial_w, alpha, iterations)
+w = gradient_descent(x_train, y_train, initial_w, alpha, iterations)
+predictions = y_hat(x_train, w)
+for i in range(len(predictions)):
+    predictions[i] = 1 if predictions[i] > 0.5 else 0
+print(f"Accuracy: {np.mean(predictions == y_train)}")
 
+
+def map_feature(X1, X2):
+    """
+    Feature mapping function to polynomial features
+    """
+    X1 = np.atleast_1d(X1)
+    X2 = np.atleast_1d(X2)
+    degree = 6
+    out = []
+    for i in range(1, degree + 1):
+        for j in range(i + 1):
+            out.append((X1 ** (i - j) * (X2**j)))
+    return np.stack(out, axis=1)
+
+
+print("Performing regularized logistic regression on ex2data2.txt")
+dataset = np.genfromtxt("data/ex2data2.txt", delimiter=",")
+x_train = dataset[:, :-1]
+x_train = map_feature(x_train[:, 0], x_train[:, 1])
+x_train = np.array([np.append(example, 1) for example in x_train])
+y_train = dataset[:, -1]
+np.random.seed(1)
+initial_w = np.random.rand(x_train.shape[1]) - 0.5
+initial_w[-1] = 1
+lambda_ = 0.01
+iterations = 10000
+alpha = 0.01
+w = gradient_descent(x_train, y_train, initial_w, alpha, iterations, lambda_)
 predictions = y_hat(x_train, w)
 for i in range(len(predictions)):
     predictions[i] = 1 if predictions[i] > 0.5 else 0
