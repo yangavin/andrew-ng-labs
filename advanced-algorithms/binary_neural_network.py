9e33af00a719bb96de9d20faa007364a21c4a0e4
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-05-13 11:27:18 -0400

    add neural network for binary classification

---
+++ advanced-algorithms/binary_neural_network.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from keras.api.models import Sequential
+from keras.api.layers import Input, Dense
+from keras.api.losses import BinaryCrossentropy
+
+
+# training data where x is a 20x20 pixel drawing, y is 0 or 1
+def load_data():
+    X = np.load("data/X.npy")
+    y = np.load("data/y.npy")
+    X = X[0:1000]
+    y = y[0:1000]
+    return X, y
+
+
+X, y = load_data()
+
+model = Sequential(
+    [
+        Input(shape=(400,)),
+        Dense(units=25, activation="sigmoid"),
+        Dense(units=15, activation="sigmoid"),
+        Dense(units=1, activation="sigmoid"),
+    ]
+)
+model.summary()
+model.compile(loss=BinaryCrossentropy(), metrics=["accuracy"])
+model.fit(X, y, epochs=20)
